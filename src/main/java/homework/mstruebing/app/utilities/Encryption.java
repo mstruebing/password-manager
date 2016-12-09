72d7e065e312051578ec92af21235623b71ecbb9
Max Strübing <struebing@sitegeist.de>
2016-12-09 14:28:26 +0100

    TASK: Add basic encryption class

---
+++ src/main/java/homework/mstruebing/app/utilities/Encryption.java
@@ -0,0 +1,30 @@
+package homework.mstruebing.app;
+
+/**
+ * Utility functions to encrypt and decrypt data
+ *
+ */
+public class Encryption 
+{
+	/**
+	 * encrypts a given string
+	 * 
+	 * @param input the string to encrypt
+	 * @return String
+	 *
+	 */
+	public static String encrypt(String input) {
+		return input;
+	}
+
+	/**
+	 * decrypts a given string
+	 *
+	 * @param input the string to decrypt
+	 * @return String
+	 *
+	 */
+	public static String decrypt(String input) {
+		return input;
+	}
+}
