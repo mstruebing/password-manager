eeedfa6c4a833a1ea7e542234a43bf423deee9d4
Max Strübing <struebing@sitegeist.de>
2017-02-17 14:40:48 +0100

    TASK: Some cleaning stuff

--- src/main/java/homework/mstruebing/app/domain/repository/UserRepository.java
+++ src/main/java/homework/mstruebing/app/domain/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package homework.mstruebing.app;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 import java.sql.ResultSet;
 
 /**
